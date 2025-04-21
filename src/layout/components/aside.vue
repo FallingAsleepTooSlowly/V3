a031eca9833410ccb5db6b5aac83de7177cbeaf3
_SSO_T_16236 <_SSO_T_16236@csair.com>
2025-04-21 17:20:27 +0800

    增加mitt插件

--- src/layout/components/aside.vue
+++ src/layout/components/aside.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="h100">
         <el-aside class="h100" style="background: purple;">
+            <el-scrollbar>
 
+            </el-scrollbar>
         </el-aside>
     </div>
 </template>
@@ -9,6 +11,7 @@
 <script setup lang="ts">
 import { useRoutesList } from '@/stores/routesList'
 import { storeToRefs } from 'pinia'
+import EventBus from '@/utils/mitt'
 
 // -------------- 定义变量
 const { routesList } = storeToRefs(useRoutesList())
---
+++ src/utils/mitt.ts
@@ -0,0 +1,3 @@
+import mitt from 'mitt'
+// mitt就是一个用来创建Mitt实例的函数，不需要重复new
+export default mitt()
