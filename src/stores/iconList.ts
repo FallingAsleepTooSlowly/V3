f62ca2b524aec59a67444fb5b37a47d577a6851c
_SSO_T_16236 <_SSO_T_16236@csair.com>
2025-05-29 16:27:43 +0800

    添加解决报错的配置

--- src/stores/iconList.ts
+++ src/stores/iconList.ts
@@ -1,18 +1,19 @@
 import { defineStore } from "pinia";
 // ---------------- 添加的图标
 import IEpUser from "~icons/ep/user";
+import IEpHouse from "~icons/ep/house";
 
 export const useIconList = defineStore('iconList', {
     state: () => ({
         // 按需加载的 icon
         iconList: {
-            'IEpFold': IEpFold,
+            'IEpHouse': IEpHouse,
             'IEpUser': IEpUser
         }
     }),
     actions: {
-        // getIcon(str: any) {
-        //     return this.iconList[str]
-        // }
+        getIcon(str) {
+            return this.iconList[str]
+        }
     }
 })
